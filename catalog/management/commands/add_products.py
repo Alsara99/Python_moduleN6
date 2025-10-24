cf6b6e24a9057b8386f4fb3e1d7d7f4091040502
Nikita <9alsara9@gmail.com>
2025-10-24 12:05:37 +0300

    patch 2

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -1,12 +1,13 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from catalog.models import Category, Product
 
 class Command(BaseCommand):
     help = 'Add test products to the database'
 
     def handle(self, *args, **kwargs):
-        products = Product.objects.all()
-        categories = Category.objects.all()
+        Product.objects.all().delete()
+        Category.objects.all().delete()
 
-        categories.delete()
-        products.delete()
+        call_command('loaddata', 'catalog_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
