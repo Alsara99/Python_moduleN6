05f384eda87c8080f18b026615ebb9f10e8c98c7
Nikita <9alsara9@gmail.com>
2025-11-07 13:47:41 +0300

    homework 24 is done

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def home(request):
-    return render(request, template_name="home.html")
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, template_name="home.html", context=context)
 
 
 def contacts(request):
     return render(request, template_name="contacts.html")
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {"product": product}
+    return render(request, template_name="product.html", context=context)
