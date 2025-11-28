c2e1e75be22c98a527fdd6be2025783a57777a43
Nikita <9alsara9@gmail.com>
2025-11-28 12:58:22 +0300

    fix

--- catalog/models.py
+++ catalog/models.py
@@ -26,10 +26,10 @@ class Product(models.Model):
         verbose_name="Цена за покупку товара",
         help_text="Введите цену за покупку товара",
     )
-    date_created = models.DateField(
+    created_at = models.DateField(
         verbose_name="Дата создания товара", help_text="Введите дату создания товара"
     )
-    date_last_changed = models.DateField(
+    updated_at = models.DateField(
         verbose_name="Дата последнего изменения товара",
         help_text="Введите дату последнего изменения товара",
     )
