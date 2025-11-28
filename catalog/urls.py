cbea16ae885b2f20b9bc1e025e3ff7c53e36d5e7
Nikita <9alsara9@gmail.com>
2025-11-28 14:13:56 +0300

    homework 26 is done

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,10 @@ from catalog.views import *
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='home'),
+    path('catalog/', ProductsListView.as_view(), name='home'),
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
     path('product_detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('catalog/new/', ProductCreateView.as_view(), name='product_create'),
+    path('catalog/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic import DetailView, ListView, TemplateView, CreateView, UpdateView, DeleteView
 from .models import Product
+from .forms import ProductForm
 
 
 class ProductsListView(ListView):
@@ -16,3 +17,23 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = "product.html"
     context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+    success_url = '/catalog/'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_form.html'
+    success_url = '/catalog/'
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "product_confirm_delete.html"
+    success_url = '/catalog/'
