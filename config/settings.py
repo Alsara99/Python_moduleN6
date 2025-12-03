9f9327ecd0fbcac56119c842c7e00c5a5c1260fd
Nikita <9alsara9@gmail.com>
2025-12-03 13:26:29 +0300

    homework 27 is done

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',
     'blogs',
+    'forms',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +141,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = "media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'nelysisgk@yandex.ru'
+EMAIL_HOST_PASSWORD = 'exeiinoiyjtaixkm'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+AUTH_USER_MODEL = 'forms.CustomUser'
